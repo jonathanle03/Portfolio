1d3c19329dceed4b32cf042879148d5b58035eb3
Jonathan Le <jonathanle062303@gmail.com>
2024-10-05 05:45:28 -0700

    Add basic about me section

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,22 @@
+import About from "@/components/About";
 import Projects from "@/components/Projects";
 import projectData from "@/data/ProjectData";
 
+const projectColors = {
+  websites: "shadow-orange-400",
+  projects: "shadow-yellow-400",
+};
+
+const projectBorders = {
+  websites: "border-orange-800",
+  projects: "border-yellow-800",
+};
+
 const Home = () => {
-  const projectColors = {
-    websites: "shadow-orange-400",
-    projects: "shadow-yellow-400",
-  };
-
-  const projectBorders = {
-    websites: "border-orange-800",
-    projects: "border-yellow-800",
-  };
-
   return (
     <div className="flex w-full justify-center">
       <div className="grid grid-cols-1 w-10/12 gap-y-4">
+        <About />
         {Object.keys(projectData).map((data, index) => (
           <Projects
             className="flex justify-around"
